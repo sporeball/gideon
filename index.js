93c48d1b9973bd3f55909e6599c597ac0a5483f8
sporeball <sporeballdev@gmail.com>
2021-06-20 14:31:58 -0700

    db + tz + ephemeral test hopefully

--- index.js
+++ index.js
@@ -9,15 +9,39 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
 
+const Conf = require('conf');
+const conf = new Conf({
+  configName: 'db',
+  cwd: '.'
+});
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
+const client = app.client;
 
 app.command('/gideon-ping', async ({ command, ack, say }) => {
   try {
     await ack();
     await say('pong!');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.command('/gideon-link', async ({ command, ack }) => {
+  try {
+    await ack();
+
+    let info = await client.users.info({ user: command.user_id });
+
+    await conf.set(`channels.${command.channel_id}.timezone`, info.tz_offset);
+    await app.client.chat.postEphemeral({
+      channel: command.channel_id,
+      user: command.user_id,
+      text: `success! my posts in this channel are now linked to your timezone (*${info.tz}*).`
+    });
   } catch (err) {
     console.error(err);
   }
